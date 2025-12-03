eed097c677316a756cdc839fbeab98fd05672efb
pchp250g8-home <pchp250g8@tutamail.com>
2025-12-03 17:42:55 +0300

    NumOfDigits

---
+++ NumOfDigits.py
@@ -0,0 +1,12 @@
+import os
+
+os.system("cls")
+print("Input an integer number:")
+iNum = int(input())
+iVal = abs(iNum)
+nDigits = 0
+while (iVal > 0):
+    iVal //= 10
+    nDigits += 1
+
+print(f"The number {iNum} has {nDigits} digits")
